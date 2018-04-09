37c6ee01e6bbdb58025375b478a2956edd848b61
Dario Campagna <campagna@esteco.com>
2018-04-09 09:11:39 +0200

    Removed duplication of test data initialization

--- test/source/it/esteco/pos/SellOneItem.java
+++ test/source/it/esteco/pos/SellOneItem.java
@@ -1,16 +1,23 @@
 package it.esteco.pos;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.mockito.Mockito.*;
 
 public class SellOneItem {
 
+    private Display display;
+    private PointOfSale pointOfSale;
+
+    @Before
+    public void setUp() throws Exception {
+        display = mock(Display.class);
+        pointOfSale = new PointOfSale(display);
+    }
+
     @Test
     public void productFound() {
-        Display display = mock(Display.class);
-        PointOfSale pointOfSale = new PointOfSale(display);
-
         pointOfSale.onBarcode("123456");
 
         verify(display).showPrice("$7.95");
@@ -18,9 +25,6 @@ public class SellOneItem {
 
     @Test
     public void anotherProductFound() {
-        Display display = mock(Display.class);
-        PointOfSale pointOfSale = new PointOfSale(display);
-
         pointOfSale.onBarcode("789987");
 
         verify(display).showPrice("$11.99");
@@ -28,9 +32,6 @@ public class SellOneItem {
 
     @Test
     public void productNotFound() {
-        Display display = mock(Display.class);
-        PointOfSale pointOfSale = new PointOfSale(display);
-
         pointOfSale.onBarcode("999999");
 
         verify(display).showProductNotFoundFor("999999");
@@ -38,9 +39,6 @@ public class SellOneItem {
 
     @Test
     public void emptyBarcode() {
-        Display display = mock(Display.class);
-        PointOfSale pointOfSale = new PointOfSale(display);
-
         pointOfSale.onBarcode("");
 
         verify(display).showEmptyBarcodeError();
